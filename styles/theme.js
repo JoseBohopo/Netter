24f31da08da80b9ad2dde32f07caac4269323cc4
Jose Bohopo <ind44280@adobe.com>
2023-02-19 17:05:33 +0100

    Add theme

---
+++ styles/theme.js
@@ -0,0 +1,18 @@
+import { Grey_Qo } from "@next/font/google"
+
+export const breakpoints = {
+    mobile: '32.5rem'
+}
+
+
+export const fonts = {
+    base: 'system-ui, -apple-system,"sans-serif", Roboto, Ubuntu'
+}
+
+export const colors = {
+    black: '#000000',
+    white: '#FFFFFF',
+    primary: '#0099ff',
+    secondary: '#1c5480',
+    lightGrey: '#fdfdfd',
+}
