dc89bc76b428021e5d7faf8ab248c231a262fb69
Jose Bohopo <ind44280@adobe.com>
2023-02-23 01:34:22 +0100

    add css tag

--- components/Layouts/styles/index.ts
+++ components/Layouts/styles/index.ts
@@ -1,27 +1,28 @@
 import { colors } from "eplant/styles/theme";
+import css from "styled-jsx/css";
 
-const styles = `
-h1 {
-  color: ${colors.primary};
-  font-weight: 800;
-  margin-bottom: 0;
-}
+const styles = css`
+  h1 {
+    color: ${colors.primary};
+    font-weight: 800;
+    margin-bottom: 0;
+  }
 
-h2 {
-  color: ${colors.secondary};
-  font-size: 18px;
-  margin: 0;
-}
-section {
-  display: grid;
-  height: 100%;
-  place-content: center;
-  place-items: center;
-  text-align: center;
-  align-content: center;
-  gap: 1rem;
-  width: 100%;
-}
+  h2 {
+    color: ${colors.secondary};
+    font-size: 18px;
+    margin: 0;
+  }
+  section {
+    display: grid;
+    height: 100%;
+    place-content: center;
+    place-items: center;
+    text-align: center;
+    align-content: center;
+    gap: 1rem;
+    width: 100%;
+  }
 `;
 
 export default styles;
