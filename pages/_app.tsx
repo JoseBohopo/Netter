4b16b2a0bb2f02b5c161ed3a55241be17b247a54
Jose Bohopo <ind44280@adobe.com>
2023-02-27 01:20:03 +0100

    Add auth provider to app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
-import 'eplant/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AuthContextProvider } from "eplant/context/authContext";
+import "eplant/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <AuthContextProvider>
+      <Component {...pageProps} />
+    </AuthContextProvider>
+  );
 }
