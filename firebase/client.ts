6c9b5e0d68a44b687355a8bed307916b1b62ef55
Jose Bohopo <ind44280@adobe.com>
2023-03-07 02:48:11 +0100

    delete sortdata

--- firebase/client.ts
+++ firebase/client.ts
@@ -78,28 +78,24 @@ export const addNettwit = async ({
   }
 };
 
-export const sortData = (querySnapshot: any) => {
-  querySnapshot.sort((a: any, b: any) => b.createdAt - a.createdAt);
-};
-
 export const fetchLatestNettwits = async () => {
   let querySnapshot;
   try {
-    querySnapshot = await getDocs(collection(db, "nettwits")).then((snapshot) =>
-      snapshot.docs.map((docs) => {
-        const data = docs.data();
+    return (querySnapshot = await getDocs(collection(db, "nettwits")).then(
+      (snapshot) =>
+        snapshot.docs.map((docs) => {
+          const data = docs.data();
 
-        const id = docs.id;
-        const { createdAt } = data;
+          const id = docs.id;
+          const { createdAt } = data;
 
-        return {
-          ...data,
-          id,
-          createdAt: +createdAt.toDate(),
-        };
-      })
-    );
-    return sortData(querySnapshot);
+          return {
+            ...data,
+            id,
+            createdAt: +createdAt.toDate(),
+          };
+        })
+    ));
   } catch (error) {
     console.log(error);
   }
