e2a26bc4d836d8ccf2168276b7cd0069bf2a88dd
Jose Bohopo <ind44280@adobe.com>
2023-03-10 11:26:19 +0100

    types coming from the database

--- components/Organisms/Nettwit/types/index.ts
+++ components/Organisms/Nettwit/types/index.ts
@@ -2,7 +2,12 @@ export interface INettwit {
   userName?: string;
   avatar?: string;
   content?: string;
-  createdAt?: string;
+  createdAt?: ICreatedAt;
   id?: number;
   img?: string | null;
+}
+
+interface ICreatedAt {
+  _nanoseconds: number;
+  _seconds: number;
 }
