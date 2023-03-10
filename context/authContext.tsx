902a8c04b00ebf6c35fc355e10098e665257bf8c
Jose Bohopo <ind44280@adobe.com>
2023-03-10 02:33:15 +0100

    remove await from unsuscribe

--- context/authContext.tsx
+++ context/authContext.tsx
@@ -25,7 +25,7 @@ export const AuthContextProvider = ({ children }: IAuthProvider) => {
     return () => {
       (async () => {
         try {
-          await unsubscribe();
+          unsubscribe();
         } catch (error) {
           console.error(error);
           setLoading(false);
