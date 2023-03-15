6d3ad5d25ed1528575b35b68e51b3ca896ecdd72
Jose Bohopo <ind44280@adobe.com>
2023-03-15 21:14:26 +0100

    add compatibility with oldest browsers and devices

--- hooks/useDateTimeFormat.tsx
+++ hooks/useDateTimeFormat.tsx
@@ -1,16 +1,30 @@
-function useDateTimeFormat(timestamp: string | Date) {
-  const date = new Date(Number(timestamp));
-  const language = navigator.language || "en-GB";
+const isDateTimeFormatSupported =
+  typeof Intl !== "undefined" && Intl.DateTimeFormat;
+export const formatDate = (timestamp: any) => {
+  const date = new Date(timestamp);
+  const language = "en-GB";
+
+  if (!isDateTimeFormatSupported) {
+    const options = {
+      weekday: "short",
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    } as const;
+    return date.toLocaleDateString(language, options);
+  }
 
   const options: Intl.DateTimeFormatOptions = {
     year: "numeric",
     month: "numeric",
     day: "numeric",
     hour: "numeric",
-    minute: "numeric",
     second: "numeric",
   };
   return new Intl.DateTimeFormat(language, options).format(date);
+};
+function useDateTimeFormat(timestamp?: any) {
+  return formatDate(timestamp);
 }
 
 export default useDateTimeFormat;
