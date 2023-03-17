9d91b6526ca6fa8821447cf7896752c08f03fcca
Jose Bohopo <ind44280@adobe.com>
2023-03-17 17:19:44 +0100

    style(Layouts): add styles for div and img elements in Layouts component

--- components/Layouts/styles/index.ts
+++ components/Layouts/styles/index.ts
@@ -23,6 +23,16 @@ const styles = css`
     gap: 1rem;
     width: 100%;
   }
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.5rem;
+  }
+
+  img {
+    width: 20%;
+  }
 `;
 
 export default styles;
