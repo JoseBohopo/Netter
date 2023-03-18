d5a74b977d950e21599e0c85af9c086465f5c64e
Jose Bohopo <ind44280@adobe.com>
2023-03-18 16:21:25 +0100

    refactor(Modal): simplify conditional rendering and class names

--- components/Molecules/Modal/index.tsx
+++ components/Molecules/Modal/index.tsx
@@ -9,29 +9,31 @@ interface IModal {
 }
 
 function Modal({ children, title, setToggle, toggle }: IModal) {
-  if (toggle) {
-    return (
-      <>
-        <section className="modal hidden">
-          {" "}
-          <div className="top">
-            <h3>{title}</h3>
-            <div className="outer" onClick={() => setToggle(false)}>
-              <div className="inner">
-                <label>Back</label>
-              </div>
+  const showSectionHiddenClassName = toggle ? "modal visible" : "modal hidden";
+  const showOverlayHiddenClassName = toggle
+    ? "overlay visible"
+    : "overlay hidden";
+
+  return (
+    <>
+      <section className={showSectionHiddenClassName}>
+        {" "}
+        <div className="top">
+          <h3>{title}</h3>
+          <div className="outer" onClick={() => setToggle(false)}>
+            <div className="inner">
+              <label>Back</label>
             </div>
           </div>
-          {children}
-        </section>
-        <div className="overlay hidden"></div>
-        <button className="btn btn-open"></button>
+        </div>
+        {children}
+      </section>
+      <div className={showOverlayHiddenClassName}></div>
+      <button className="btn btn-open"></button>
 
-        <style jsx>{styles}</style>
-      </>
-    );
-  }
-  return <></>;
+      <style jsx>{styles}</style>
+    </>
+  );
 }
 
 export default Modal;
